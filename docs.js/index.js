22a1058b5dde97c66041ef4f201ce18e9e6e6ae3
Miguel Ruiz <later481@gmail.com>
2023-01-31 22:24:12 -0600

    gsap animation completion

--- docs.js/index.js
+++ docs.js/index.js
@@ -1,4 +1,4 @@
-gsap.registerPlugin(ScrollTrigger);
+/* gsap.registerPlugin(ScrollTrigger);
 
 gsap.utils.toArray('.section').forEach(section => {
     ScrollTrigger.create({
@@ -7,4 +7,31 @@ gsap.utils.toArray('.section').forEach(section => {
         pin: true,
         pinSpacing: false
     });
+}); */
+
+let sections = gsap.utils.toArray("section"),
+    currentSection = sections[0];
+
+gsap.defaults({overwrite: 'auto', duration: 0.4});
+
+// stretch out the body height according to however many sections there are. 
+gsap.set("body", {height: (sections.length * 100) + "%"});
+
+// create a ScrollTrigger for each section
+sections.forEach((section, i) => {
+    ScrollTrigger.create({
+    // use dynamic scroll positions based on the window height (offset by half to make it feel natural)
+    start: () => (i - 0.5) * innerHeight,
+    end: () => (i + 0.5) * innerHeight,
+    // when a new section activates (from either direction), set the section accordinglyl.
+    onToggle: self => self.isActive && setSection(section)
+    });
 });
+
+function setSection(newSection) {
+    if (newSection !== currentSection) {
+    gsap.to(currentSection, {scale: 0.6, autoAlpha: 0})
+    gsap.to(newSection, {scale: 1, autoAlpha: 1});
+    currentSection = newSection;
+    }
+}
