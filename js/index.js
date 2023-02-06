b58cb429d6bd626a18d2771f2c9f57b44309a7fb
Miguel Ruiz <later481@gmail.com>
2023-02-05 22:55:21 -0600

    section home

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ gsap.utils.toArray('.section').forEach(section => {
 let sections = gsap.utils.toArray("section"),
     currentSection = sections[0];
 
-gsap.defaults({overwrite: 'auto', duration: 0.4});
+gsap.defaults({overwrite: 'auto', duration: 0.3});
 
 // stretch out the body height according to however many sections there are. 
 gsap.set("body", {height: (sections.length * 100) + "%"});
@@ -21,8 +21,8 @@ gsap.set("body", {height: (sections.length * 100) + "%"});
 sections.forEach((section, i) => {
     ScrollTrigger.create({
     // use dynamic scroll positions based on the window height (offset by half to make it feel natural)
-    start: () => (i - 0.5) * innerHeight,
-    end: () => (i + 0.5) * innerHeight,
+    start: () => (i - .6) * innerHeight,
+    end: () => (i + 0.6) * innerHeight,
     // when a new section activates (from either direction), set the section accordinglyl.
     onToggle: self => self.isActive && setSection(section)
     });
@@ -30,8 +30,11 @@ sections.forEach((section, i) => {
 
 function setSection(newSection) {
     if (newSection !== currentSection) {
-    gsap.to(currentSection, {scale: 0.6, autoAlpha: 0})
+    gsap.to(currentSection, {scale: .2, autoAlpha: 0})
     gsap.to(newSection, {scale: 1, autoAlpha: 1});
     currentSection = newSection;
     }
 }
+
+
+
